03a909fcd373023983ae597b328e89ff952c55b3
Letícia <38981626+LeehCoelhoo@users.noreply.github.com>
2021-08-12 20:47:37 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lista_atividadee/view/lista_atividade.dart';
+void main(){
+  runApp((MaterialApp(
+    theme: ThemeData(
+        primaryColor: Colors.amber[900],
+      accentColor: Colors.blueAccent[700],
+      buttonTheme: ButtonThemeData(
+      buttonColor: Colors.blueAccent[700],
+      textTheme: ButtonTextTheme.primary
+      )
+          ),
+        home: Home(),
+      )
+      ),
+  );
+}
